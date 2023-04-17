4dabb5be2c479ef7cf36905836b47f84683a84c8
Leorb12 <leorb1212bs@gmail.com>
2023-04-17 15:55:50 -0600

    Added endpoints for msapi, 'connect' still missing

--- app.py
+++ app.py
@@ -1,23 +1,37 @@
 from flask import Flask, request, jsonify
 import sqlite3
 
-# Connect to the SQLite database
-conn = sqlite3.connect('msgapi.db')
-cursor = conn.cursor()
 
-# Execute an SQL query
-cursor.execute('SELECT * FROM Messages')
-rows = cursor.fetchall()
+app = Flask(__name__)
 
-# Process the fetched rows
-for row in rows:
-    # Access columns by index (0-based) or by name
-    id = row[0]
-    sender = row[1]
-    recipient = row[2]
-    message = row[3]
-    datetime = row[4]
-    print(f'id: {id}, sender: {sender}, recipient: {recipient}, message: {message}, datetime: {datetime}')
 
-# Close the database connection
-conn.close()
+@app.route('/send', methods=['POST'])
+def send_message():
+    data = request.get_json()
+    sender = data['sender']
+    recipient = data['recipient']
+    message = data['message']
+
+    conn = sqlite3.connect('msgapi.db')
+    c = conn.cursor()
+
+    c.execute('INSERT INTO messages (sender, recipient, message) VALUES (?, ?, ?)', (sender, recipient, message))
+    conn.commit()
+
+    conn.close()
+
+    return jsonify({'message': 'Message sent successfully!'})
+
+
+@app.route('/inbox/<recipient>', methods=['GET'])
+def get_inbox(recipient):
+    conn = sqlite3.connect('msgapi.db')
+    c = conn.cursor()
+
+    c.execute('SELECT sender, message FROM messages WHERE recipient = ?', (recipient,))
+    messages = c.fetchall()
+
+    conn.close()
+
+    return jsonify([{'sender': message[0], 'message': message[1]} for message in messages])
+
