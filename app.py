235b0602fbe95084297ef13fcfe44d5e906667b8
Leorb12 <leorb1212bs@gmail.com>
2023-04-18 12:30:27 -0600

    Error handling added

--- app.py
+++ app.py
@@ -10,31 +10,38 @@ if __name__ == '__main__':
 
 @app.route('/send', methods=['POST'])
 def send_message():
-    data = request.get_json()
-    sender = data['sender']
-    recipient = data['recipient']
-    message = data['message']
+    try:
+        data = request.get_json()
+        sender = data['sender']
+        recipient = data['recipient']
+        message = data['message']
 
-    conn = sqlite3.connect('msgapi.db')
-    c = conn.cursor()
+        conn = sqlite3.connect('msgapi.db')
+        c = conn.cursor()
 
-    c.execute('INSERT INTO messages (sender, recipient, message) VALUES (?, ?, ?)', (sender, recipient, message))
-    conn.commit()
+        c.execute('INSERT INTO messages (sender, recipient, message) VALUES (?, ?, ?)', (sender, recipient, message))
+        conn.commit()
 
-    conn.close()
+        conn.close()
 
-    return jsonify({'message': 'Message sent successfully!'})
+        return jsonify({'message': 'Message sent successfully!'}), 201
+
+    except sqlite3.Error as e:
+        return jsonify({'error': str(e)}), 500
 
 
 @app.route('/inbox/<recipient>', methods=['GET'])
 def get_inbox(recipient):
-    conn = sqlite3.connect('msgapi.db')
-    c = conn.cursor()
+    try:
+        conn = sqlite3.connect('msgapi.db')
+        c = conn.cursor()
 
-    c.execute('SELECT sender, message FROM messages WHERE recipient = ?', (recipient,))
-    messages = c.fetchall()
+        c.execute('SELECT sender, message FROM messages WHERE recipient = ?', (recipient,))
+        messages = c.fetchall()
 
-    conn.close()
+        conn.close()
 
-    return jsonify([{'sender': message[0], 'message': message[1]} for message in messages])
+        return jsonify([{'sender': message[0], 'message': message[1]} for message in messages]), 200
 
+    except sqlite3.Error as e:
+        return jsonify({'error': str(e)}), 500
